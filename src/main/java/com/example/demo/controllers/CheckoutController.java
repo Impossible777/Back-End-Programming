2bd5a3a5dd49fb604505c42e450b6a637a22e313
Admin <mcrist1@wgu.edu>
2023-11-07 12:29:02 -0800

    Task H is complete

--- src/main/java/com/example/demo/controllers/CheckoutController.java
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -6,6 +6,7 @@ import com.example.demo.dto.PurchaseResponse;
 import com.example.demo.services.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
+//Task H
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
